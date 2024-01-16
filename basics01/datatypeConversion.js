ecae05ea48bee6d0fa2ceba2b26b5b9917aefb77
ashu1205 <100120507+ashu1205@users.noreply.github.com>
2024-01-16 12:25:54 +0000

    conversion of datatypes

---
+++ basics01/datatypeConversion.js
@@ -0,0 +1,39 @@
+let score =null
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NaN  , type - number
+//undefined , "ashu" => NaN , type - number
+//true=> 1 ; false=> 0 
+
+// except for null => 0 , type - number
+
+// let loggedIn=1; //=> true
+// let loggedIn=""; => false 
+// let loggedIn="ashu"; =>true 
+// let loggedIn=null; => false
+// let loggedIn=undefined;  => false
+// let loggedIn=NaN; => false 
+let loggedIn=[];
+
+
+let booleanLoggedIn= Boolean(loggedIn);
+
+// console.log(booleanLoggedIn);
+
+// console.log(""==false);  => true ---> value of "" is false
+// console.log([]=="");  ---> look for this ???
+
+let num= 45
+
+let stringNum=String(num)
+
+// console.log(stringNum);  converted into "45" 
+// console.log(typeof(stringNum));  string
