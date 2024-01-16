dfcefb81ba55cf9096e17c4747aa4c78621868b8
ashu1205 <100120507+ashu1205@users.noreply.github.com>
2024-01-16 08:06:24 +0000

    datatypes- null,undefined

---
+++ basics01/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // to treat all code as newer version of js
+
+// alert("warning you are in node")  note : you are in node env , not in browser
+
+
+let name="ashutosh"
+let age =20
+let loggedIn=true 
+
+// number => range --> 2^53  
+// String => " " , ' '
+//boolean =>true/false
+// null => standalone value --> null means empty value 
+// eg: let str=""  --> is not null because type is already defined as String
+//undefined => value is not assigned
+
+//symbol => unique identification
+
+//object 
+
+console.log(typeof null); // object
+console.log(typeof undefined);  //undefined
